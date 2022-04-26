57071cb68cc0e691ed0f1bd5fbccaf8927478fb2
Pratikpawar39 <pratikpawar392000@gmail.com>
2022-04-26 10:58:29 +0530

    added logic for not accept duplicate data at backend

--- server/routes/faq.js
+++ server/routes/faq.js
@@ -2,24 +2,45 @@ const router = require('express').Router();
 const Faq = require('../models/faqModel');
 
 //apis
-router.route('/').get((req, res)=> {
+//--> fetch FAQ from mongo
+router.route('/').get((req, res) => {
     Faq.find()
-    .then(faq => res.json(faq))
-    .catch(err => res.status(400).json('Error: ' + err))
+        .then(faq => res.json(faq))
+        .catch(err => res.status(400).json('Error: ' + err))
 })
 
+//--> create FAQ
 router.route('/add').post((req, res) => {
-    const faq = req.body.faq;
+    const category = req.body.category;
+    const question = req.body.question;
+    const answer = req.body.answer;
+
+
+    //fetch faq questions for check
+    User.find({}, { question: 1, _id: 0 })
+        .then(questions => {
+            console.log("first", questions)
+
+            //check duplicate question
+            questions.map(ele => {
+                if (ele.question === question) {
+                    console.log("Duplicate Question!");
+                    return res.status(404).json(`Error: ${question} this question is already exist!`);
+                }
+            })
+        })
+        .catch(err => res.status(400).json('Error: ' + err))
 
     const newFaq = new Faq({
         category,
         question,
         answer
     });
-    
+
+    //save faq details
     newFaq.save()
-    .then(() => res.json('Faq created!'))
-    .catch((err) => res.status(400).json('Error: ' + err))
+        .then(() => res.json('Faq created!'))
+        .catch((err) => res.status(400).json('Error: ' + err))
 })
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 let User = require('../models/userMode');
 
 //apis
+//--> fetch users api
 router.route('/').get((req, res) => {
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+//--> create user api
 router.route('/add').post((req, res) => {
   console.log("Add user")
   const username = req.body.username;
@@ -15,21 +17,37 @@ router.route('/add').post((req, res) => {
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
 
-  //check confirm password
-  if(confirmPassword === password){
+  //fetch usernames for check
+  User.find({}, { username: 1, _id: 0 })
+    .then(usernames => {
+      console.log("first", usernames)
+
+      //check duplicate username
+      usernames.map(ele => {
+        if (ele.username === username) {
+          console.log("Duplicate User!");
+          return res.status(404).json(`Error: ${username} is already exist!`);
+        }
+      })
+    })
+    .catch(err => res.status(400).json('Error: ' + err))
+
+  //check confirm password and add new user
+  if (confirmPassword === password) {
     const newUser = new User({
       username,
       name,
       password
     });
 
+    //save userDetails in mongo
     newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-  }else{
+      .then(() => res.json('User added!'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  } else {
     res.status(400).json('Password Not matched!');
   }
-  
+
 });
 
 module.exports = router;
